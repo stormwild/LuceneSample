a4ed9c2edbfdae7816b5f7c196e2be7e90a7621e
Alexander R. Torrijos <artorrijos@gmail.com>
2020-07-26 19:58:01 +0800

    Updates

---
+++ src/main/java/app/lucene/MergeSort.java
@@ -0,0 +1,17 @@
+package app.lucene;
+
+import java.util.*;
+
+public class MergeSort {
+    public static void mergeSort(int[] list, int len) {
+        if(len < 2) {
+            return;
+        }
+        int mid = len / 2;
+        int[] left = list[mid];
+    }
+
+    public static void merge() {
+
+    }
+}
---
+++ src/test/java/app/MergeSortTest.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class MergeSortTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testMergeSort() {
+        // Arrange
+
+        // Act
+
+        // Assert
+        assertTrue(true);
+    }
+}
